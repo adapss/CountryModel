f524da749af28706841dc83b7b22b6975dc773ab
Sal Spada <sspada@arcweb.com>
2025-05-11 08:11:16 -0400

    Version 2

---
+++ pages/Forecast.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from myCountryModelPackages.Economic_Research import *
+
+
+st.title("Country Model Forecast Analysis")
+st.write('Place holder page for analysis of the forecast from the generated country model.  ')
---
+++ pages/MarketShares.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from myCountryModelPackages.Economic_Research import *
+
+
+st.title("Country Model Market Share Analysis")
+st.write('Place holder page for analysis of the market shares from the generated country model.  ')
